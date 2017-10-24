b2923221a2706335883aa191e9c8da8d8f938aa7
ConnorJang <connorjang20@gmail.com>
2017-10-23 20:29:44 -0700

    Initial Commit

---
+++ Midterm/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Midterm
+//
+//  Created by iMac03 on 2017-10-23.
+//  Copyright © 2017 iMac03. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
